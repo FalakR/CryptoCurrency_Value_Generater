3dc2f2ea3330e23cbcf466d81b5ebc82a7adc476
falakrastogi <falakrast1@gmail.com>
2024-05-20 17:37:21 +0530

    cryto_reader

---
+++ crypto_scraper.py
@@ -0,0 +1,47 @@
+import requests
+import os
+from prettytable import PrettyTable  # pip install prettytable
+
+# ---------SELF NOTE--------
+# req = requests.get("https://iexcloud.io").text  # allows us to look at the following url.
+#
+# print(req)  # the html code would be displayed on printing the text attribute.
+
+api_key = os.environ.get("api_key")
+
+
+class CryptoCurrency:
+    base_url = "https://cloud.iexapis.com/stable/crypto"
+    prices = []
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.add_prices_to_list()
+
+    @property
+    def complete_url(self):
+        return f"{CryptoCurrency.base_url}/{self.symbol}/price?token={api_key}"
+
+    @property
+    def price(self):
+        req = requests.get(self.complete_url).json()  # This method will convert the JSON data to a Python dictionary
+        return float(req.get('price'))
+
+    def add_prices_to_list(self):
+        CryptoCurrency.prices.append(
+            [self.price, self.symbol]
+        )
+
+    @staticmethod
+    def prices_table():
+        pt = PrettyTable(["Prices", "Crypto Name"])
+        pt.add_rows(CryptoCurrency.prices)
+        return pt
+
+    @staticmethod
+    def clean_prices():
+        CryptoCurrency.prices.clear()
+
+    @staticmethod
+    def show_prices():
+        print(CryptoCurrency.prices_table())
